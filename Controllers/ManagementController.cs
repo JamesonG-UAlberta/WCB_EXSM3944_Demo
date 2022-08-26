ba3a8d5c2123936885bde551df6546a6ca5c6d4b
James Grieve <jgrieve@ualberta.ca>
2022-08-26 10:35:02 -0600

    Adding to the database;

--- Controllers/ManagementController.cs
+++ Controllers/ManagementController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using MVC_Demo.Data;
+using MVC_Demo.Models;
 
 namespace MVC_Demo.Controllers
 {
@@ -23,6 +24,24 @@ namespace MVC_Demo.Controllers
             ViewBag.TestBag = "Here's some data I put in the ViewBag!";
             ViewData["TestData"] = "Here's some data I put in ViewData!";
             return View();
+        }
+
+        public ActionResult DoTheGeneration(string customerId, string productId, string qty)
+        {
+            Invoice newInvoice = new Invoice()
+            {
+                Customerid = int.Parse(customerId)
+            };
+            _context.Invoices.Add(newInvoice);
+            _context.SaveChanges();
+            _context.InvoiceProducts.Add(new InvoiceProduct()
+            {
+                Orderid = newInvoice.Id,
+                Inventoryid = int.Parse(productId),
+                Quantity = int.Parse(qty)
+            });
+            _context.SaveChanges();
+            return RedirectToAction("GenerateOrder");
         }
     }
 }
