a8030e58a9befc488ccf5bdc5ec8294c90cdcfcf
James Grieve <jgrieve@ualberta.ca>
2022-08-29 09:36:28 -0600

    Staging controller method for validation;

--- Controllers/ManagementController.cs
+++ Controllers/ManagementController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using MVC_Demo.Data;
 using MVC_Demo.Models;
+using MVC_Demo.Models.Exceptions;
 
 namespace MVC_Demo.Controllers
 {
@@ -17,6 +18,9 @@ namespace MVC_Demo.Controllers
 
         public ActionResult GenerateOrder()
         {
+            // If there are exceptions, store them in the view data/bag so we can inform the user about them.
+            ViewData["Exceptions"] = TempData["Exceptions"];
+
             ViewData["Customers"] = new SelectList(_context.Customers, "Id", "Fullname");
             ViewData["Products"] = new SelectList(_context.Products, "Id", "Name");
 
@@ -28,19 +32,35 @@ namespace MVC_Demo.Controllers
 
         public ActionResult DoTheGeneration(string customerId, string productId, string qty)
         {
-            Invoice newInvoice = new Invoice()
+            // Let's do some validation!
+            BLLValidationException validationState = new BLLValidationException();
+
+            // Do validation, if something fails, add it as a sub exception.
+
+
+
+            if (validationState.Any)
             {
-                Customerid = int.Parse(customerId)
-            };
-            _context.Invoices.Add(newInvoice);
-            _context.SaveChanges();
-            _context.InvoiceProducts.Add(new InvoiceProduct()
+                TempData["Exceptions"] = validationState;
+
+            }
+            // If we go in here, we've not encountered any validation issues, so we are good to proceed.
+            else
             {
-                Orderid = newInvoice.Id,
-                Inventoryid = int.Parse(productId),
-                Quantity = int.Parse(qty)
-            });
-            _context.SaveChanges();
+                Invoice newInvoice = new Invoice()
+                {
+                    Customerid = int.Parse(customerId)
+                };
+                _context.Invoices.Add(newInvoice);
+                _context.SaveChanges();
+                _context.InvoiceProducts.Add(new InvoiceProduct()
+                {
+                    Orderid = newInvoice.Id,
+                    Inventoryid = int.Parse(productId),
+                    Quantity = int.Parse(qty)
+                });
+                _context.SaveChanges();
+            }
             return RedirectToAction("GenerateOrder");
         }
     }
